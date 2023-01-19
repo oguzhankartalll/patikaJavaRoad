f289dd9592cd04c323cc710a45a76ee53a5854f3
oguzhan kartal <oguzzhann.kartal@gmail.com>
2023-01-19 22:28:04 +0300

    eklendi

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		int mat,fizik,kimya,turkce,tarih,muzik,toplam;
+
+		Scanner kb = new Scanner(System.in);
+
+		System.out.print("Matematik Notunuzu Giriniz:");
+		mat = kb.nextInt();
+
+		System.out.print("Fizik Notunuzu Giriniz:");
+		fizik = kb.nextInt();
+
+		System.out.print("Kimya Notunuzu Giriniz:");
+		kimya = kb.nextInt();
+
+		System.out.print("Türkçe Notunuzu Giriniz:");
+		turkce = kb.nextInt();
+
+		System.out.print("Tarih Notunuzu Giriniz:");
+		tarih = kb.nextInt();
+
+		System.out.print("Müzik Notunuzu Giriniz:");
+		muzik = kb.nextInt();
+
+		toplam = mat + fizik + kimya + turkce + tarih + muzik;
+
+		double result = toplam / 6.0;
+
+
+		System.out.println(result > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı");
+
+
+	}
+}
